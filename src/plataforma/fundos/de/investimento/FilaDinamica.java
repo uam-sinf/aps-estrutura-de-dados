0bee2b6a52cf81b2fb6b55cdc314af58b5dd58d3
NathFriederichs <33435242+NathFriederichs@users.noreply.github.com>
2018-10-25 19:22:42 -0300

    Update FilaDinamica.java

--- src/plataforma/fundos/de/investimento/FilaDinamica.java
+++ src/plataforma/fundos/de/investimento/FilaDinamica.java
@@ -2,25 +2,100 @@ package plataforma.fundos.de.investimento;
 
 public class FilaDinamica {
     
+    private No inicio, fim;
+    private int quantidade = 0;
+    
+    public FilaDinamica(){
+
+        inicio = fim = null;
+        quantidade = 0;
+
+    }
+    
     public boolean isEmpty() {
-        return true;
+        
+        if(quantidade == 0){
+
+            return true;
+
+        }
     }
     
     public void enqueue(float dado) {
+        
+        No novoNo = new No(dado);
+
+        if(isEmpty()) {
+
+            inicio = fim = novoNo;
+
+        }
+        else {
+
+            fim.next = novoNo;
+            fim = novoNo
+
+        }
+
+        quantidade++;
         
     }
     
     public float dequeue() {
         throw new IndexOutOfBoundsException(); // Erro para quando não existem mais elementos
+        
+        if(isEmpty()) {
+
+            return -1;
+        }
+        else {
+
+            No temp = inicio;
+
+            if(inicio == fim) {
+
+                inicio = fim = null;
+
+            }
+            else {
+
+                inicio = inicio.next;
+
+            }
+            quantidade--;
+            return temp.dado;
+
+        }
     }
     
     public float front() {
         throw new IndexOutOfBoundsException(); // Erro para quando não existem mais elementos
+        
+        if(inicio != null) {
+            
+            
+        }
+        
     }
     
     public String imprimir() {
-        String txt = "";
         
+        if(isEmpty()) {
+
+            return "Não há investimentos!"
+
+        }
+        else {
+            String txt = "";
+            No atual = inicio;
+
+            while(atual != null) {
+
+                saida += atual.imprimir() + " ";
+                atual = atual.next;
+
+            }
+        }
         return txt;
     }
 }
