6167d74c51375d67d9370879c72730f00e21b888
NathFriederichs <33435242+NathFriederichs@users.noreply.github.com>
2018-10-25 19:12:49 -0300

    Update No.java

--- src/plataforma/fundos/de/investimento/No.java
+++ src/plataforma/fundos/de/investimento/No.java
@@ -1,5 +1,15 @@
 package plataforma.fundos.de.investimento;
 
 public class No {
+
+    public int investimento;
+    public No prox;
+
+    public No(int invest){
+
+        investimento = invest;
+        next = null;
+
+    }
     
 }
